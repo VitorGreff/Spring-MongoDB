abe1f6bee51006755cbb2a6eaafbea3c56739083
VitorGreff <vitorgreffdias@gmail.com>
2023-08-28 22:53:13 -0300

    first commit

---
+++ src/test/java/com/greff/mongoworkshop/MongoWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greff.mongoworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoWorkshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
