775c1c06f3602cecb10040c6feb405e520515b88
VitorGreff <vitorgreffdias@gmail.com>
2023-08-30 11:10:53 -0300

    config rep created

---
+++ src/main/java/com/greff/mongoworkshop/config/Instantiation.java
@@ -0,0 +1,24 @@
+package com.greff.mongoworkshop.config;
+
+import com.greff.mongoworkshop.Repositories.UserRepository;
+import com.greff.mongoworkshop.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository repo;
+    @Override
+    public void run(String... args) throws Exception {
+        repo.deleteAll();
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+        repo.saveAll(Arrays.asList(maria,alex,bob));
+    }
+}
